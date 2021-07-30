13aef24f551b3cbdca51b10e4bc3fc5d68fd45a5
Sonishraj <49720238+Sonishraj@users.noreply.github.com>
2021-07-30 19:35:58 +0530

    Add files via upload

---
+++ config_twitter.py
@@ -0,0 +1,10 @@
+## Twitter API credentials.
+
+# Consumer keys (API key and secret)
+consumer_key = ''
+consumer_secret = ''
+
+# Application access token key and access token secret
+access_token = ''
+access_token_secret = ''
+
